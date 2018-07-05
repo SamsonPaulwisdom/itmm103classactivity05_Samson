4a796c017e2b16ba9f45c1a09600dcc532187ae4
itmm103samsonpaulwisdom <itmm103samsonpaulwisdom>
2018-07-05 11:28:50 +0800

    Fruit List activity

---
+++ Activity5a/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	
+<?php
+
+$fruits = array (
+
+array("Apples", "Malus domestica", "105"),
+array("Banana", "Musa acuminata", "105"),
+array("Blackberries", "Rubus", "100"),
+array("Apricots", "Prunus armeniac", "101"),
+);
+
+
+echo "<table border = 1 align = 'center'>";
+for ($i = 0; $i < 4; $i++){
+
+	echo "<tr>";
+
+	for ($j = 0; $j < 3; $j++) { 
+		echo "<td>";
+		echo " <" . $fruits[$i][$j];
+		echo "</td>";
+	}
+
+	echo "<br>";
+
+echo "</tr>";
+}
+
+echo "</table>";
+
+?>
+
+
+
+
+</body>
+</html>
