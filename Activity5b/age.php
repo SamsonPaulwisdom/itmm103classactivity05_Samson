6a8a521eed34d30f7d33f03a72aadae975a01bb3
itmm103samsonpaulwisdom <itmm103samsonpaulwisdom>
2018-07-05 11:50:38 +0800

    activity 5b

---
+++ Activity5b/age.php
@@ -0,0 +1,16 @@
+<?php
+
+$month = $_POST['month'];
+$day = $_POST['day'];
+$year = $_POST['year'];
+
+
+$monthavailable = 12;
+
+
+
+echo $month;
+
+
+
+?>
---
+++ Activity5b/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<form action = "age.php" method="post">
+	
+
+	Input birthdate : <input type="date" name="month">
+	<br>
+
+	<input type="submit">
+
+</form>
+
+
+
+
+
+
+
+</body>
+</html>
